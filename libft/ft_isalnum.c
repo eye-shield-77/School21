184ac9ecb15e726fc902aa4de751aab58e8ca0cc
EyeShield77 <eyeshield77@gmail.com>
2020-05-27 17:53:26 +0300

    tomac10

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: btaxider <eyeshield77@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/16 03:30:24 by btaxider          #+#    #+#             */
-/*   Updated: 2020/05/18 17:29:11 by btaxider         ###   ########.fr       */
+/*   Updated: 2020/05/27 17:48:36 by btaxider         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 
 int		ft_isalnum(int c)
 {
-	if (ft_isalpha(c) || ft_isdigit(c))
-		return (1);
-	return (0);
+	return (ft_isalpha(c) + ft_isdigit(c));
 }
